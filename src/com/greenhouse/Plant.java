a780635c89d24372459ead161d460e6e88699341
JonasHal <jon123jon5@hotmail.com>
2020-10-01 13:02:30 +0200

    Lidt om planter

---
+++ src/com/greenhouse/Plant.java
@@ -0,0 +1,13 @@
+package com.greenhouse;
+
+public class Plant {
+    public char plantType;
+    public int plantAge;
+}
+
+public class PlantTypes {
+    public int TEMP;
+    public int HUMIDITY;
+    public char season;
+    public int optimalGrowth;
+}
