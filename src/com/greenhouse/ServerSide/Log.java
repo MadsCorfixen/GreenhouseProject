77a18df1232aae1441f638727376050905a09d93
moleseaau <56633631+moleseaau@users.noreply.github.com>
2020-10-28 15:39:38 +0100

    hej

--- src/com/greenhouse/ServerSide/Log.java
+++ src/com/greenhouse/ServerSide/Log.java
@@ -3,7 +3,18 @@ package com.greenhouse;
 import java.time.LocalDate;
 
 public class Log {
-    double temperature;
-    LocalDate dateTime;
-    double groundHumidity;
+    private double temperature;
+    private LocalDate dateTime;
+    private double groundHumidity;
+
+    public Double getTemp() {
+        return temperature;
+    }
+
+    public void setTemp( Double newTemp) {
+        if (NewTemp < 0)
+            throw new IllegalArgumentException("Temperature cant be set to negative degrees");
+        else
+            temperature = newTemp;
+    }
 }
