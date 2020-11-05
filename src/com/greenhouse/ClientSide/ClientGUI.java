bcb71dadfca10aa577686cd4587ae54a7f882f80
Mads Corfixen <mcorfi16@student.aau.dk>
2020-11-05 08:43:14 +0100

    Update ClientGUI.java

--- src/com/greenhouse/ClientSide/ClientGUI.java
+++ src/com/greenhouse/ClientSide/ClientGUI.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ClientGUI extends JFrame implements ActionListener{
+public class ClientGUI extends JFrame implements ActionListener {
 
     private JFrame mainWindow;
     private JButton alterPlantButton;
