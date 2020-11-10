f32ea09ce0f166f265045ebadabb4cd6866ad56f
Mads Corfixen <mcorfi16@student.aau.dk>
2020-11-10 13:12:11 +0100

    Documentation for Plant class

--- src/com/greenhouse/ServerSide/Plant.java
+++ src/com/greenhouse/ServerSide/Plant.java
@@ -9,7 +9,12 @@ public class Plant implements Serializable {
     private LocalDate harvestDate;
     private String plantType;
 
-    // This is the constructor for the a plant object. Inputs are from the ones above.
+    /**
+     * The constructor for a plant object
+     * @param plantType: A string determining which plant type is to be creates, e.g. tomato or potato
+     * @param harvestDate: A date at which the plant is fully grown
+     * @param preferredTemperature: The temperature at which the plant grows optimally
+     */
     public Plant (String plantType, LocalDate harvestDate, int preferredTemperature) {
         this.plantType = plantType;
         this.harvestDate = harvestDate;
@@ -17,11 +22,19 @@ public class Plant implements Serializable {
     }
 
 
-    //Following are getters and setters for the class Plant variables
+    /**
+     * A getter for the temperature
+     * @return The preferred temperature for the plant
+     */
     public int getPreferredTemperature(){
         return preferredTemperature;
     }
 
+    /**
+     * A setter for the preferred temperature
+     * @param newPreferredTemperature: An integer determining the new preferred temperature
+     * @throws IllegalArgumentException if the temperature is below freezing.
+     */
     public void setPreferredTemperature (int newPreferredTemperature){
         if (newPreferredTemperature < 0){
             throw new IllegalArgumentException("Temperature must be above 0 degrees");
@@ -31,18 +44,35 @@ public class Plant implements Serializable {
         }
     }
 
+    /**
+     * A getter for the plant type
+     * @return The plant type of the plant
+     */
     public String getPlantType() {
         return plantType;
     }
 
+    /**
+     * A setter for the plant type
+     * @param newPlantType The new plant type
+     */
     public void setPlantType(String newPlantType) {
         this.plantType = newPlantType;
     }
 
+    /**
+     * A getter for the harvest date
+     * @return the harvest date for the plant
+     */
     public LocalDate getHarvestDate() {
         return harvestDate;
     }
 
+    /**
+     * A setter for the harvest date
+     * @param newHarvestDate
+     * @throws IllegalArgumentException if harvest date is set in the past
+     */
     public void setHarvestDate(LocalDate newHarvestDate) {
         if (newHarvestDate.compareTo(LocalDate.now()) <= 0){
             throw new IllegalArgumentException("Date is not available");
