c8ccc1c92239f799e857b1d67bd52f6b79e0626f
Mads Corfixen <mcorfi16@student.aau.dk>
2020-11-12 13:39:09 +0100

    Rollback

--- src/com/greenhouse/ServerSide/Server.java
+++ src/com/greenhouse/ServerSide/Server.java
@@ -56,12 +56,12 @@ public class Server implements Serializable{
 
             String requestType = (String)inStream.readObject();
 
-            if(!alreadyExecuted) {
-                if (listOfPlants.getIfHarvestable() > 0) {
-                    outStream.writeObject("Harvegetable");
-                alreadyExecuted = true;
-                }
-            }
+//            if(!alreadyExecuted) {
+//                if (listOfPlants.getIfHarvestable() > 0) {
+//                    outStream.writeObject("Harvegetable");
+//                alreadyExecuted = true;
+//                }
+//            }
 
 
             if (requestType.equals("addPlant")) {
