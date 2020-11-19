4a6d981a416073755f1332fe00ad9fddfa2dde67
Divixity <Theede19@student.aau.dk>
2020-11-19 09:14:51 +0100

    Bedre navngivning

--- src/com/greenhouse/ServerSide/Log.java
+++ src/com/greenhouse/ServerSide/Log.java
@@ -6,7 +6,7 @@ import java.util.logging.*;
 
 public class Log {
     public Logger logger;
-    public FileHandler fh;
+    public FileHandler fileHandler;
     private final static String file_name = "log.txt";
 
     /**
@@ -16,16 +16,16 @@ public class Log {
     public Log() throws IOException {
 
         // Checking if the logging file does not exist if not create new file
-        File f = new File(file_name);
-        if(!f.exists()){
-            f.createNewFile();
+        File file = new File(file_name);
+        if(!file.exists()){
+            file.createNewFile();
         }
 
         // Writes new log and appends it to the existing log in the right format
-        fh = new FileHandler(file_name, true);
+        fileHandler = new FileHandler(file_name, true);
         logger = Logger.getLogger("log");
-        logger.addHandler(fh);
+        logger.addHandler(fileHandler);
         SimpleFormatter formatter = new SimpleFormatter();
-        fh.setFormatter(formatter);
+        fileHandler.setFormatter(formatter);
     }
 }
